5cef2b8d7daf34779cc5596a07ea80df45792468
jugglergirl20 <76530845+jugglergirl20@users.noreply.github.com>
2021-11-30 17:17:34 -0600

    Update CancelController.java

--- src/main/java/com/example/commerce/controller/CancelController.java
+++ src/main/java/com/example/commerce/controller/CancelController.java
@@ -1,7 +1,7 @@
-package com.example.commerce.controller;
-
+import com.example.commerce.repository.JPACubicleRepository;
+import com.example.commerce.repository.JPAReservationRepository;
+import com.example.commerce.service.ReservationService;
 import org.springframework.stereotype.Controller;
-
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -11,4 +11,16 @@ public class CancelController
     public String login(){
         return "commerce/user-cancel";
     }
+
+    private final JPACubicleRepository cubicleRepository;
+    private final JPAReservationRepository reservationRepository;
+    private final ReservationService reservationService;
+
+    public CancelController(JPACubicleRepository cubicleRepository, JPAReservationRepository reservationRepository, ReservationService reservationService) {
+        this.cubicleRepository = cubicleRepository;
+        this.reservationRepository = reservationRepository;
+        this.reservationService = reservationService;
+    }
 }
+
+
