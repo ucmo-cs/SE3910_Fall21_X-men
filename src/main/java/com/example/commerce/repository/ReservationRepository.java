3a1c433e0dfb876457a513fb4afc3a32a73553bb
MarkN313 <83818635+MarkN313@users.noreply.github.com>
2021-12-01 05:34:36 -0600

    Update ReservationRepository.java

--- src/main/java/com/example/commerce/repository/ReservationRepository.java
+++ src/main/java/com/example/commerce/repository/ReservationRepository.java
@@ -1,9 +1,14 @@
 package com.example.commerce.repository;
 
+import com.example.commerce.domain.Member;
 import com.example.commerce.domain.ReservationsTable;
+
+import java.util.List;
 
 public interface ReservationRepository
 {
     ReservationsTable saveReservation(ReservationsTable reservationsTable);
     int cancelReservation(String id);
+
+    List<ReservationsTable> findAll();
 }
