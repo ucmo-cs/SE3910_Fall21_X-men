ecfef021fe3a88cc8402ee41f3a71ecd45c4e074
MarkN313 <83818635+MarkN313@users.noreply.github.com>
2021-12-01 05:31:13 -0600

    Update ViewController.java
    wrong file updated. just rolling back

--- commerce/src/main/java/com/example/commerce/controller/ViewController.java
+++ commerce/src/main/java/com/example/commerce/controller/ViewController.java
@@ -1,31 +1,14 @@
 package com.example.commerce.controller;
 
-import com.example.commerce.domain.Member;
-import com.example.commerce.domain.ReservationsTable;
-import com.example.commerce.service.ReservationService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
 
 @Controller
 public class ViewController
 {
-    private final ReservationService reservationService;
-
-    @Autowired
-    public ViewController(ReservationService reservationService){
-        this.reservationService = reservationService;
-    }
-
-    @GetMapping("main/view")
-    public String list(Model model){
-        List<ReservationsTable> reservations = reservationService.findReservations();
-        model.addAttribute("reservations" , reservations);
-
-        return "commerce/user-view";
+    @GetMapping("login")
+    public String login(){
+        return "commerce/login";
     }
 }
